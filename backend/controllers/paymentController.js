77d11b409f5f0babd39fa64de71f3efb40fc4824
Codebakerzs <codebakerz3@gmail.com>
2022-07-01 23:46:34 +0530

    gg

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,21 +1,53 @@
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const router = require("express").Router();
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
-exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: "inr",
-    metadata: {
-      company: "Ecommerce",
-    },
-  });
+router.post("/orders", async (req, res) => {
+	try {
+		const instance = new Razorpay({
+			key_id: process.env.KEY_ID,
+			key_secret: process.env.KEY_SECRET,
+		});
 
-  res
-    .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret });
+		const options = {
+			amount: req.body.amount * 100,
+			currency: "INR",
+			receipt: crypto.randomBytes(10).toString("hex"),
+		};
+
+		instance.orders.create(options, (error, order) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).json({ message: "Something Went Wrong!" });
+			}
+			res.status(200).json({ data: order });
+		});
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error!" });
+		console.log(error);
+	}
 });
 
-exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+router.post("/verify", async (req, res) => {
+	try {
+		const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+			req.body;
+		const sign = razorpay_order_id + "|" + razorpay_payment_id;
+		const expectedSign = crypto
+			.createHmac("sha256", process.env.KEY_SECRET)
+			.update(sign.toString())
+			.digest("hex");
+
+		if (razorpay_signature === expectedSign) {
+			return res.status(200).json({ message: "Payment verified successfully" });
+		} else {
+			return res.status(400).json({ message: "Invalid signature sent!" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error!" });
+		console.log(error);
+	}
 });
+
+module.exports = router;
